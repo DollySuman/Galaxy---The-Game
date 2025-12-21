16a5c5ebf9c7b942ea8ee6a59c1ab6f252cc2527
Dolly Suman <dolly.suman.023@gmail.com>
2025-12-21 10:11:17 +0530

    react app

---
+++ galaxy/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+  const [value,setvalue] = useState(0)
+  return (
+    <div className="App">
+      {value}
+    </div>
+  );
+}
+
+export default App;
